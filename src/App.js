7da51081516be8b5a31340e56d42f92e0f2c76b7
burak <99646235+Burrakkozan@users.noreply.github.com>
2022-09-01 02:36:03 +0300

    some message here

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-
-
 import {
   BrowserRouter as Router,
   Route,
@@ -36,7 +34,6 @@ function App() {
     <Router>
       
       <Preloader load={load} />
-      
       
       <div className="App" id={load ? "no-scroll" : "scroll"}  
       >
